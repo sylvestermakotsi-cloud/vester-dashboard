2bdfef70599a367e8d6622fe665ec7c04ccf0efa
sylvester <sylvestermakotsi@gmail.com>
2026-08-29 00:17:19 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,35 +1,49 @@
 <?php
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $tel = $_POST['tel'];
-    $nationality = $_POST['nationality'];
-    $address = $_POST['address'];
-    $county = $_POST['county'];
-    $company = $_POST['company'];
-    $desc = $_POST['desc'];
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    exit('Method not allowed.');
+}
 
+$required_fields = array('fname', 'lname', 'email', 'tel', 'nationality', 'address', 'county', 'company', 'desc');
+$fields = array();
 
-    $email_from = 'info@vesterhelp.com';
-    $email-body = "First Name: $fname.\n".
-                  "Last Name: $lname.\n".
-                  "Email Address: $email.\n".
-                  "Phone Number: $tel.\n".
-                  "Nationality: $nationality.\n".
-                  "Address: $address.\n".
-                  "County: $county.\n".
-                  "Company: $company.\n".
-                  "Project Description: $desc.\n";
+foreach ($required_fields as $field) {
+    $value = isset($_POST[$field]) ? trim((string) $_POST[$field]) : '';
 
-    $to = "sylvestermakotsi@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $email \r\n";
+    if ($value === '') {
+        http_response_code(400);
+        exit('Please complete all required fields.');
+    }
 
+    $fields[$field] = $value;
+}
 
-    mail($to,$email_subject,$email_body,$headers);
-    header("Location: index.html");
+if (!filter_var($fields['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400);
+    exit('Please provide a valid email address.');
+}
 
+$to = 'sylvestermakotsi@gmail.com';
+$subject = 'New website contact form submission';
+$message = "First Name: {$fields['fname']}\n"
+    . "Last Name: {$fields['lname']}\n"
+    . "Email Address: {$fields['email']}\n"
+    . "Phone Number: {$fields['tel']}\n"
+    . "Nationality: {$fields['nationality']}\n"
+    . "Address: {$fields['address']}\n"
+    . "County: {$fields['county']}\n"
+    . "Company: {$fields['company']}\n"
+    . "Project Description: {$fields['desc']}\n";
 
+$headers = "From: info@vester-two.com\r\n"
+    . "Reply-To: {$fields['email']}\r\n"
+    . "Content-Type: text/plain; charset=UTF-8\r\n";
 
+if (!mail($to, $subject, $message, $headers)) {
+    http_response_code(500);
+    exit('The message could not be sent. Please try again later.');
+}
 
+header('Location: index.html?sent=1');
+exit;
 ?>
